51fc87a4ec61c4975e177261b94d66567e6090fd
Marawan Saqr <loew.krs@yahoo.com>
2025-02-27 02:37:25 +0200

    Clean Some Folders

--- Js/Person/Three/Project/custom.js
+++
@@ -1,6 +0,0 @@
-const basePerson = { nationality: "Egyptian" };
-
-let person1 = Object.assign({}, basePerson, { name: "Marawan", age: 28, job: "Front-End Developer"});
-
-console.log(person1);
-console.log(basePerson);
--- Js/User/Project/custom.js
+++
@@ -1,8 +0,0 @@
-const user = {
-  username: "ali_dev",
-  "account-type": "premium",
-};
-
-
-console.log(user.username);
-console.log(user["account-type"]);
